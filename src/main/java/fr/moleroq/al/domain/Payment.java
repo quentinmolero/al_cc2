a78fec5013a604a31df3f3c7b471ba638e630b50
Quentin MOLERO <quentin.molero@estiam.com>
2021-12-17 13:55:27 +0100

    Refactored Payment fields to add date

--- src/main/java/fr/moleroq/al/domain/Payment.java
+++ src/main/java/fr/moleroq/al/domain/Payment.java
@@ -1,15 +1,17 @@
 package fr.moleroq.al.domain;
 
+import java.util.Date;
+
 public class Payment {
 
     private final PaymentCredentials paymentCredentials;
 
-    private Payment(long amount, PaymentId paymentId) {
-        this.paymentCredentials = PaymentCredentials.of(amount, paymentId);
+    private Payment(long amount, Date date, PaymentId paymentId) {
+        this.paymentCredentials = PaymentCredentials.of(amount, date, paymentId);
     }
 
-    public static Payment of(long amount, PaymentId paymentId) {
-        return new Payment(amount, paymentId);
+    public static Payment of(long amount, Date date, PaymentId paymentId) {
+        return new Payment(amount, date, paymentId);
     }
 
     public long getAmount() {
--- src/main/java/fr/moleroq/al/domain/PaymentCredentials.java
+++ src/main/java/fr/moleroq/al/domain/PaymentCredentials.java
@@ -1,21 +1,31 @@
 package fr.moleroq.al.domain;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PaymentCredentials {
 
     private final long amount;
+    private final Date date;
     private final PaymentId paymentId;
 
-    private PaymentCredentials(long amount, PaymentId paymentId) {
+    private PaymentCredentials(long amount, Date date, PaymentId paymentId) {
         this.amount = amount;
+        this.date = date;
         this.paymentId = paymentId;
     }
 
-    public static PaymentCredentials of (long amount, PaymentId paymentId) {
-        return new PaymentCredentials(amount, paymentId);
+    public static PaymentCredentials of (long amount, Date date, PaymentId paymentId) {
+        return new PaymentCredentials(amount, date, paymentId);
     }
 
     public long getAmount() {
         return amount;
+    }
+
+    public String getPaymentDate() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z");
+        return simpleDateFormat.format(this.date);
     }
 
     public PaymentId getPaymentId() {
