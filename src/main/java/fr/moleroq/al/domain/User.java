986cdf53256accba8b8d7aaa5c663f37f1635188
Quentin MOLERO <quentin.molero@estiam.com>
2021-12-20 16:51:24 +0100

    Connect state to User

--- src/main/java/fr/moleroq/al/domain/User.java
+++ src/main/java/fr/moleroq/al/domain/User.java
@@ -3,17 +3,29 @@ package fr.moleroq.al.domain;
 public final class User {
 
     private final UserCredentials userCredentials;
+    private final UserStateHistory history;
 
-    private User(UserId userId, String lastname, String firstname, String password) {
+    private User(UserId userId, String lastname, String firstname, String password, UserState initialState) {
         this.userCredentials = UserCredentials.of(userId, lastname, firstname, password);
+        this.history = UserStateHistory.create(initialState);
     }
 
     public static User of(UserId userId, String lastname, String firstname, String password) {
-        User user = new User(userId, lastname, firstname, password);
+        User user = new User(userId, lastname, firstname, password, UserState.IN_CREATION);
         if (ValidationUserEngine.getInstance().test(user)) {
+            user.changeState(UserState.CREATED);
             return user;
         }
+        user.changeState(UserState.CREATED_WITH_ERROR);
         throw new IllegalArgumentException("Illegal arguments");
+    }
+
+    public void changeState(UserState userState) {
+        this.history.append(userState);
+    }
+
+    public void changePassword(String newPassword) {
+        this.userCredentials.changePassword(newPassword);
     }
 
     public UserId getUserId() {
@@ -30,10 +42,6 @@ public final class User {
 
     public String getUserPassword() {
         return this.userCredentials.getPassword();
-    }
-
-    public void changePassword(String newPassword) {
-        this.userCredentials.changePassword(newPassword);
     }
 
     @Override
